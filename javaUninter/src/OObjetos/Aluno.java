a5c616ccf671f56e128e6d97956aa6370783953e
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2024-02-20 21:25:02 -0300

    JavaUninter/construtores

--- javaUninter/src/OObjetos/Aluno.java
+++ javaUninter/src/OObjetos/Aluno.java
@@ -6,6 +6,14 @@ public class Aluno {
 	String nome;
 	String cpf;
 	
+	Aluno() {};
+	
+	Aluno(int pMatricula, String pNome, String pCpf) {
+		matricula = pMatricula;
+		nome = pNome;
+		cpf = pCpf;
+	}
+	
 	//criar um metodo(imprimir)
 	void info() {
 		System.out.println("Matricula: " + matricula);
--- javaUninter/src/OObjetos/PrincipalTurma.java
+++ javaUninter/src/OObjetos/PrincipalTurma.java
@@ -8,11 +8,27 @@ public class PrincipalTurma {
 		// TODO Auto-generated method stub
 		Turma nova = new Turma();
 		
+		Aluno a = new Aluno();
+		a.cpf="111.222.333.-4";
+		a.nome="Super lucas";
+		a.matricula=1001;
+		
+		a.info();
+		Aluno b = new Aluno(1002,"Super Luigi", "222.333.444.5");
+		
+	    b.info();
+				
+				
+				
+				
+				
 		nova.prof = new Professor();
 		nova.prof.nome="Sivaldo";
 		nova.alunos= new ArrayList();
 		nova.alunos.add(new Aluno());
 		nova.alunos.get(0).nome="super Lucas";
+		
+		
 		
 
 	}
