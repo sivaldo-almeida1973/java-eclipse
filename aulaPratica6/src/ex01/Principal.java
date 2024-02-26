c959972f6a229f44de385789007d876540522598
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2024-02-25 21:15:14 -0300

    JavaUninter/aula pratica 6/ex02

--- aulaPratica6/src/ex01/Principal.java
+++ aulaPratica6/src/ex01/Principal.java
@@ -5,20 +5,19 @@ import java.util.Scanner;
 public class Principal {
 
 	public static void main(String[] args) {
+		
+		Scanner teclado = new Scanner(System.in);
+		// criar uma conta
+		Conta c1 = new Conta("Mario", 5000);	
+		Conta c2 = new Conta("Luigi", 10000);
+		c1.info();
+		c2.info();
+		
+		System.out.println("Digite um valor para Transferir c1 -> c2: ");
+		double valorTransferir = teclado.nextDouble();
 		boolean sucessoLeitura = false;
 		
 	    while(!sucessoLeitura) {
-	    	
-			Scanner teclado = new Scanner(System.in);
-			// criar uma conta
-			Conta c1 = new Conta("Mario", 5000);	
-			Conta c2 = new Conta("Luigi", 10000);
-			c1.info();
-			c2.info();
-			
-			System.out.println("Digite um valor para Transferir c1 -> c2: ");
-			double valorTransferir = teclado.nextDouble();
-			
 	    	  try {
 	    			
 	  			c1.transferir(valorTransferir, c2);
---
+++ projetoArrayP6/src/projetoArrayP6/Principal.java
@@ -0,0 +1,32 @@
+package projetoArrayP6;
+
+public class Principal {
+	
+	public static int[] instanciaArray(int n) {
+		return new int[n];
+	}
+
+	public static void main(String[] args) {
+		// 
+		int tam=-10;
+		int arr[];
+		
+		try {
+			int conta = 10/0;
+			arr = instanciaArray(tam);
+			for(int i= 0 ;i< tam; i++) {
+				System.out.println(arr[i]);
+			}
+		} catch (NegativeArraySizeException e) {
+			System.out.println("Valor inválido, digite outro valor!");
+			System.out.println(e.getMessage());
+		}
+		catch(ArithmeticException e) {
+			System.out.println("Divisão por zero!");
+			
+		}
+		
+
+	}
+
+}
