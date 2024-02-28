828f12cdcc14576c7b3a9a64ca185638c7f64cb9
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2024-02-28 12:53:39 -0300

    JavaUninter/ tratamento

---
+++ projetoBanco/src/projetoBanco/Conta.java
@@ -0,0 +1,48 @@
+package projetoBanco;
+
+import javax.management.RuntimeErrorException;
+
+public class Conta {
+	String nome;
+	double saldo;
+	
+	public Conta(String nome, double saldo) {
+		super();
+		this.nome = nome;
+		this.saldo = saldo;
+	}
+	//não vai retornar nenhum valor, mas recebe parametro
+	void depositar(double valor) {
+		if(valor < 0 ) {
+			throw new RuntimeException("Valor inválido");
+		}
+		saldo += valor;
+	}
+	//não vai retornar nenhum valor, mas recebe parametro
+	void sacar(double valor) {
+		if(valor > saldo) {
+			throw new RuntimeException("Saldo insuficiente");
+		}
+		if(valor < 0 ) {
+			throw new RuntimeException("Valor inválido");
+		}
+		saldo -= valor;
+	}
+	
+	void transferir(double valor, Conta destino) {
+		this.sacar(valor);
+		destino.depositar(valor);
+	}
+	
+	
+	
+	
+	void info() {
+		
+		System.out.println("Nome:" + nome);
+		System.out.println("Saldo:" + saldo);
+		System.out.println("----------");
+	}
+	
+
+}
